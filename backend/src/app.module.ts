bddb557366d7ca854587878acacf8f31f2948af5
Guilherme Ferreira <guifs2@hotmail.com>
2023-01-06 08:20:44 -0300

    Imports associate module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AssociateController } from './modules/associate/associate.controller';
+import { AssociateService } from './modules/associate/associate.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, AssociateController],
+  providers: [AppService, AssociateService],
 })
 export class AppModule {}
