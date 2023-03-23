0def904433c79a0f0b8070e33ef552719a33d0c8
luisabrito <90068390+luisabritod@users.noreply.github.com>
2023-03-23 15:46:42 -0300

    final commit
    Co-authored-by: Lucas_Nunes <LucasNunesSv@users.noreply.github.com>

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,21 +1,36 @@
-import "./styles/inicial.css";
-import logoEscrita from '../assets/bookClubLogo.png'
-import background from '../assets/background.png'
-import styles from '../pages/styles/Home.css'
+import "./styles/Home.css";
+import background from "../assets/background.png";
+import styles from "../pages/styles/Home.css";
 import Navbar from "../components/Navbar";
+import { useNavigate } from "react-router-dom";
 
-function Home(){
-return(
-    
+function Home() {
+  const navigate = useNavigate();
+
+  const goCreate = () => {
+    navigate("/");
+  };
+
+  return (
     <div class="container">
-        <Navbar />
-        <div className={styles.Homemain} class="Homemain">
-            <div class="back">
-                <img className={styles.image} src={background} alt="" />
-                </div>
+      <Navbar />
+      <div className={styles.Homemain} class="Homemain">
+        <div class="botao-div-home">
+          <strong>
+            Book Club
+            <br />
+            Management System
+          </strong>
+          <button type="button" class="botao-home" onClick={goCreate}>
+            Logout
+          </button>
         </div>
+        <div class="back">
+          <img className={styles.image} src={background} alt="" />
+        </div>
+      </div>
     </div>
-)
+  );
 }
 
-export default Home
+export default Home;
--- frontend/src/pages/Livros.js
+++ frontend/src/pages/Livros.js
@@ -1,32 +1,32 @@
-import * as React from 'react';
+import * as React from "react";
 import "./styles/inicial.css";
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import { createTheme, styled } from '@mui/material/styles';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
-import { useNavigate } from 'react-router-dom';
-import Navbar from '../components/Navbar';
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import { createTheme, styled } from "@mui/material/styles";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import { useNavigate } from "react-router-dom";
+import Navbar from "../components/Navbar";
 import { useState, useEffect } from "react";
-import { api } from '../service/api';
+import { api } from "../service/api";
 
 const tema = createTheme({
   palette: {
     primary: {
-      main: '#221f28',
-      light: '#eae8ed' ,
+      main: "#221f28",
+      light: "#eae8ed",
     },
     secondary: {
-      main: '#fff',
-      dark: '#000'
+      main: "#fff",
+      dark: "#000",
     },
   },
 });
 
-const StyledTableCell = styled(TableCell)(({ theme}) => ({
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
     backgroundColor: tema.palette.primary.main,
     color: tema.palette.secondary.main,
@@ -39,79 +39,85 @@ const StyledTableCell = styled(TableCell)(({ theme}) => ({
 }));
 
 const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  '&:nth-of-type(odd)': {
+  "&:nth-of-type(odd)": {
     backgroundColor: tema.palette.primary.light,
   },
-  '&:last-child td, &:last-child th': {
+  "&:last-child td, &:last-child th": {
     border: 0,
   },
 }));
 
+function Livros() {
+  const [livros, setLivro] = useState([
+    {
+      id: "",
+      title: "",
+      author: "",
+      publisher: "",
+      description: "",
+    },
+  ]);
 
-function Livros(){
-  const [livros, setLivro] = useState([{
-    id: "",
-    title: "",
-    author: "",
-    publisher: "",
-    description: "",
-  }]);
+  const navigate = useNavigate();
 
-  const navigate = useNavigate()
-  
   const goCreate = () => {
-    navigate('/livros/cadastro');
-  }
+    navigate("/livros/cadastro");
+  };
 
   useEffect(() => {
-    api.get("books").then(response => {
+    api.get("books").then((response) => {
       setLivro(response.data);
     });
-  }, [])
+  }, []);
 
-  return(
-    
-  <div class="Container">
-    <Navbar />
-  <div class="Main">
-  <div class='botao-div'>
-      <button type="button" class='botao' onClick={goCreate}>
-        Cadastrar Livro
-      </button>
+  return (
+    <div class="Container">
+      <Navbar />
+      <div class="Main">
+        <div class="botao-div">
+          <button type="button" class="botao" onClick={goCreate}>
+            Cadastrar Livro
+          </button>
+        </div>
+        <div class="tabela">
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 800 }} aria-label="customized table">
+              <TableHead>
+                <TableRow>
+                  <StyledTableCell>ID</StyledTableCell>
+                  <StyledTableCell align="left">TÍTULO</StyledTableCell>
+                  <StyledTableCell align="left">AUTOR</StyledTableCell>
+                  <StyledTableCell align="left">EDITORA</StyledTableCell>
+                  <StyledTableCell align="left">DESCRIÇÃO</StyledTableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {livros.map((livro) => (
+                  <StyledTableRow key={livro.id}>
+                    <StyledTableCell component="th" scope="row">
+                      {livro.id}
+                    </StyledTableCell>
+                    <StyledTableCell align="left">
+                      {livro.title}
+                    </StyledTableCell>
+                    <StyledTableCell align="left">
+                      {livro.author}
+                    </StyledTableCell>
+                    <StyledTableCell align="left">
+                      {livro.publisher}
+                    </StyledTableCell>
+                    <StyledTableCell align="left">
+                      {livro.description}
+                    </StyledTableCell>
+                  </StyledTableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </div>
+      </div>
     </div>
-    <div class="tabela">
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 800 }} aria-label="customized table">
-        <TableHead>
-          <TableRow>
-            <StyledTableCell>ID</StyledTableCell>
-            <StyledTableCell align="left">TÍTULO</StyledTableCell>
-            <StyledTableCell align="left">AUTOR</StyledTableCell>
-            <StyledTableCell align="left">EDITORA</StyledTableCell>
-            <StyledTableCell align="left">DESCRIÇÃO</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {livros.map((livro) => (
-            <StyledTableRow key={livro.id}>
-              <StyledTableCell component="th" scope="row">
-                {livro.id}
-              </StyledTableCell>
-              <StyledTableCell align="left">{livro.title}</StyledTableCell>
-              <StyledTableCell align="left">{livro.author}</StyledTableCell>
-              <StyledTableCell align="left">{livro.publisher}</StyledTableCell>
-              <StyledTableCell align="left">{livro.description}</StyledTableCell>
-            </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-    </div>
-  </div>
-
-</div>
   );
 }
-
 
 export default Livros;
